64e4445f1fde3028d5827a9829d8bd07bf09e591
Brandon Calloway <brandon@scullytown.com>
2009-03-11 10:05:33 -0400

    added thoughtbot plugins and gems, removed rpec

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -27,10 +27,28 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "capistrano-ext", :lib => "capistrano"
-  config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
-  config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  
+  config.gem 'capistrano-ext',
+              :lib => 'capistrano'
+  config.gem "haml",
+             :version    => "2.0.9"
+  config.gem 'RedCloth',
+              :lib => 'redcloth', 
+              :version => '~> 4.0.3'
+  config.gem 'mislav-will_paginate', 
+              :lib => 'will_paginate', 
+              :source => 'http://gems.github.com', 
+              :version => '~> 2.3.7'
+  config.gem 'rubyist-aasm', 
+              :lib => 'aasm',
+              :source => 'http://gems.github.com',
+              :version => '2.0.5'
+  # Thoughtbot gems
+  config.gem "thoughtbot-factory_girl",
+              :lib    => "factory_girl",
+              :source => "http://gems.github.com"
+  config.gem "thoughtbot-shoulda",
+              :lib => "shoulda",
+              :source => "http://gems.github.com" 
   # These cause problems with irb. Left in for reference
   # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
   # config.gem 'rspec', :lib => 'spec', :version => '1.1.11'
